7569e881aab51889f1b69ebd602749d7d71eabc3
Mateuzo <mateushcandido5@gmail.com>
2024-05-13 16:45:00 -0300

    implementando testes unitários para verificar validação de padrão de cpf durante criação de uma entidade Usuario

---
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -0,0 +1,36 @@
+package br.com.alura.codechella.domain.entities.usuario;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class UsuarioTest {
+    @Test
+    public void naoDeveCadastrarUsuarioComCpfNoFormatoInvalido(){
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("123456789-99", "mateus@email.com", LocalDate.parse("1999-10-17"), "Mateus"));
+
+       Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("123456.789-99", "mateus@email.com", LocalDate.parse("1999-10-17"), "Mateus"));
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("123.456789-99", "mateus@email.com", LocalDate.parse("1999-10-17"), "Mateus"));
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("", "mateus@email.com", LocalDate.parse("1999-10-17"), "Mateus"));
+    }
+
+    @Test
+    public void deveCadastrarUsuarioComCpfNoFormatoCorreto(){
+        String mensagem = "";
+        try{
+            Usuario usuario = new Usuario("123.456.789-99", "mateus@email.com", LocalDate.parse("1999-10-17"), "Mateus");
+            mensagem = "usuario criado";
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
+
+        Assertions.assertTrue("usuario criado".equals(mensagem));
+    }
+}
