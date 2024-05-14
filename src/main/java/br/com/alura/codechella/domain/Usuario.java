9f599ac34c26a63dc43a1054ca3506aa18e1c74b
Mateuzo <mateushcandido5@gmail.com>
2024-05-14 10:33:08 -0300

    implementando endereco e criando uma factory de usuario

--- src/main/java/br/com/alura/codechella/domain/Usuario.java
+++ src/main/java/br/com/alura/codechella/domain/Usuario.java
@@ -1,6 +1,9 @@
-package br.com.alura.codechella.domain.entities.usuario;
+package br.com.alura.codechella.domain;
+
+import br.com.alura.codechella.domain.entities.usuario.Endereco;
 
 import java.time.LocalDate;
+
 
 public class Usuario {
     private String cpf;
@@ -8,17 +11,31 @@ public class Usuario {
     private LocalDate nascimento;
     private String email;
 
+    private Endereco endereco;
+
 
     public Usuario(String cpf, String nome, LocalDate nascimento, String email) {
-        if (cpf == null || !cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
-            throw new IllegalArgumentException("Cpf no padrão incorreto!");
-        }
-
+        validarCpf(cpf);
 
         this.cpf = cpf;
         this.nome = nome;
         this.nascimento = nascimento;
         this.email = email;
+    }
+
+    public void validarCpf(String cpf){
+        if (cpf == null || !cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("Cpf no padrão incorreto!");
+        }
+    }
+
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void definirEndereco(Endereco endereco) {
+        this.endereco = endereco;
     }
 
     public String getCpf() {
